209cf3fa4b2f4f27fcb36eb26f83a5caef5f0abb
HERMINDER SINGH <hermindersingh12345@gmail.com>
2019-06-17 22:30:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sunshine/recycleview/Movies.kt
@@ -0,0 +1,7 @@
+package com.example.sunshine.recycleview
+
+
+data class Movies(val movieName:String,
+                  val actorName:String,
+                  val yearOfRelease:String,
+                  val imageName:Int)
